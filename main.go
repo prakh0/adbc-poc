6c35924b2c53f77c8a2ce4c79edd07bab0de93b9
prakhar <prakhar_p@outlook.com>
2026-02-24 19:25:35 +0530

    Initial Commit

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/apache/arrow-adbc/go/adbc"
 	"github.com/apache/arrow-adbc/go/adbc/drivermgr"
 )
 
@@ -26,26 +27,45 @@ func main() {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
 	stmt, err := conn.NewStatement()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
+
 	stmt.SetSqlQuery("SELECT * FROM airportdb.booking")
 	stmt.SetOption("adbc.statement.batch_size", "250000")
 	startTime := time.Now()
+
 	result, _, err := stmt.ExecuteQuery(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer result.Release()
-	batchNum := 0
-	for result.Next() {
-		batchNum++
-		batch := result.RecordBatch()
-		fmt.Printf("Batch #%d: %d rows\n", batchNum, batch.NumRows())
-		batch.Release()
+
+	ingestConn, err := db.Open(context.Background())
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer ingestConn.Close()
+
+	rowInserted, err := adbc.IngestStream(
+		context.Background(),
+		ingestConn,
+		result,
+		"booking_copy",
+		"adbc.ingest.mode.replace",
+		adbc.IngestStreamOptions{
+			Extra: map[string]string{
+				"adbc.statement.ingest.batch_size": "12000",
+			},
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Number of rows inserted: %d\n", rowInserted)
 	fmt.Printf("Total time for reading data: %v\n", time.Since(startTime))
 	fmt.Println("Completed successfully!")
 }
